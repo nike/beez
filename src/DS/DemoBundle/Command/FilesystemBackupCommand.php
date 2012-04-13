a642617fd267e04db18d23883c2df0240cc6ab2b
Nicola Piras <nicola.piras.xyz@gmail.com>
2012-04-13 21:41:35 +0200

    added filesystem:backup command

--- src/DS/DemoBundle/Command/FilesystemBackupCommand.php
+++ src/DS/DemoBundle/Command/FilesystemBackupCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Demo\Command;
+namespace DS\DemoBundle\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -24,22 +24,14 @@ class FilesystemBackupCommand extends Command
 
   protected function execute(InputInterface $input, OutputInterface $output)
   {
-    $source = realpath($input->getArgument('source'));
-    $destination = realpath($input->getArgument('destination'));
+    $source = preg_replace('/\/+$/', '', $input->getArgument('source'));
+    $destination = preg_replace('/\/+$/', '', $input->getArgument('destination'));
     
     $archiveName = sprintf('%s-%s.tar.gz', basename($source), date('YmdHi', time()));
     $archivePath = dirname($source);
     
-//    if(!is_dir($destinationDir)) {
-//      if(!$createDestinationDir) 
-//        throw new InvalidArgumentException("Archive directory not found: " . $destinationDir);
-//      
-//      $this->getFileSystem()->mkdir($destinationDir, true);
-//    }
     if (!is_dir($destination)) {
-      $output->writeln('<error>TODO: get a normalized path when destination does not exist</error>');
-      die;
-      mkdir($destination, 0777, true);
+      @mkdir($destination, 0777, true);
     }
     
     // Options:
@@ -49,8 +41,6 @@ class FilesystemBackupCommand extends Command
     // f: archive to file
     // C: root dir in the archived file
     $commandLine = sprintf('tar -czf %s/%s %s -C"%s"', $destination, $archiveName, $source, $archivePath);
-//    $output->writeln(sprintf('<info>%s</info>', $commandLine));
-//    return 0;
 
     $process = new Process($commandLine);
     
