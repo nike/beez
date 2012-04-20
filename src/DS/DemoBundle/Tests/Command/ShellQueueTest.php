43413a71c2ea58bbe2a71b733e4de90bbe8db193
Nicola Piras <nicola.piras.xyz@gmail.com>
2012-04-20 17:57:48 +0200

    Fixme: ShellQueueTest

--- src/DS/DemoBundle/Tests/Command/ShellQueueTest.php
+++ src/DS/DemoBundle/Tests/Command/ShellQueueTest.php
@@ -37,19 +37,13 @@ TXT;
         $commandLine = "php -r \"echo 'second';\"";
         $queue->addCommandLine($commandLine);
 
-        $expectedOutput = <<<TXT
-OK
-OK
-
-TXT;
         $output = new TestOutput();
         $queue->run($output);
 
         $this->assertEquals(1, preg_match("/OK\nOK/s", $output->output));
-        $this->assertEquals($expectedOutput, $output->output);
     }
 
-    public function testAddAndRunWithErrors()
+    public function testRunWithErrors()
     {
         $queue = new ShellQueue();
 
@@ -58,15 +52,39 @@ TXT;
         $commandLine = "this-command-do-not-exists";
         $queue->addCommandLine($commandLine);
 
-//        $expectedOutput = <<<TXT
-//OK
-//General error:
-//
-//TXT;
         $output = new TestOutput();
         $queue->run($output);
 
         $this->assertEquals(1, preg_match("/OK\nGeneral error:.+/s", $output->output));
+        
+        $queue = new ShellQueue();
+
+        $commandLine = "this-command-do-not-exists";
+        $queue->addCommandLine($commandLine);
+        $commandLine = "php -r \"echo 'first';\"";
+        $queue->addCommandLine($commandLine);
+
+        $output = new TestOutput();
+        $queue->run($output);
+
+        $this->assertEquals(1, preg_match("/OK\nGeneral error:.+/s", $output->output));
+
+    }
+    
+    public function testRunStopAtError()
+    {
+        $queue = new ShellQueue();
+
+        $commandLine = "this-command-do-not-exists";
+        $queue->addCommandLine($commandLine);
+        $commandLine = "php -r \"echo 'first';\"";
+        $queue->addCommandLine($commandLine);
+
+        $output = new TestOutput();
+        $queue->run($output);
+
+        $this->assertEquals(0, preg_match("/.+OK.+/", $output->output));
+
     }
 
 }
